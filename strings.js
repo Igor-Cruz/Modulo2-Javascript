8a1b5825b2f126beb7534603dbf4c9a29070d33f
IGOR CRUZ <igorcruz.it@outlook.com>
2021-12-06 08:20:12 -0300

    Strings and method

---
+++ strings.js
@@ -0,0 +1,19 @@
+const nome = 'Igor Cruz'
+
+//Method
+//Split separa
+//join concatena
+console.log(nome.split(' ').join('<===>'))
+
+//interpolação de string
+const opa = `
+okay
+Nome: ${nome}
+blz
+`
+console.log(opa)
+
+const outra = nome + ' ' + opa
+console.log(outra)
+
+
